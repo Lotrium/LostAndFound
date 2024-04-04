8864a2a534d8206fca716e2c3bfac3b4511b3bfc
Lotrium <131715429+Lotrium@users.noreply.github.com>
2024-04-04 09:43:19 +0200

    0.2 - Pełne GUI do mapy, przyciski funkcjonalne - markery, utwórz ścieżkę, dark mode, exit.

--- main.py
+++ main.py
@@ -1,75 +1,91 @@
-import tkinter as tk
-import geopy.geocoders as geocoders
-import requests
-from tkintermapview import TkinterMapView
-from geopy.exc import GeocoderUnavailable
+import tkintermapview as tkmap
+import customtkinter as ctk
+
+markers = []
+paths = []
 
 
-def search_location():
-    try:
-        geolocator = geocoders.Nominatim(user_agent="your_app_name")
-        location = geolocator.geocode("Your search")
-        if location is not None:
-            map_widget.set_position(location.latitude, location.longitude)
-        else:
-            show_error_message("Location not found. Please try again with a different search query.")
-    except GeocoderUnavailable as e:
-        print(f"Error: {e}")
-        show_error_message("Failed to connect to the geocoding service. Please try again later.")
-    except requests.exceptions.ConnectionError as e:
-        print(f"Error: {e}")
-        show_error_message("Failed to connect to the geocoding service. Please try again later.")
+def lookup():
+    lookup_dialog = ctk.CTkInputDialog(text="Type in the address you want to look up:", title="My Map")
+    map_widget.set_address(lookup_dialog.get_input())
 
 
-def show_error_message(message):
-    error_window = tk.Toplevel(root_tk)
-    error_window.title("Error")
-    error_label = tk.Label(error_window, text=message)
-    error_label.pack()
-    ok_button = tk.Button(error_window, text="OK", command=error_window.destroy)
-    ok_button.pack()
+def zoom(value):
+    map_widget.set_zoom(value)
 
 
-def open_second_window():
-    second_window = tk.Toplevel(root_tk)
-    second_window.geometry("300x200")
-    second_window.title("Search Radius")
-
-    # create slider
-    slider = tk.Scale(second_window, from_=0, to=100, orient=tk.HORIZONTAL, label="Search Radius (km)")
-    slider.pack()
-
-    # create search button
-    search_button = tk.Button(second_window, text="Search", command=lambda: search_location_with_radius(slider.get()))
-    search_button.pack()
+def marker():
+    marker_dialog = ctk.CTkInputDialog(text="Type in the name of the marker:", title="My Map")
+    current_pos = map_widget.get_position()
+    markers.append(map_widget.set_marker(current_pos[0], current_pos[1], text=marker_dialog.get_input()))
 
 
-def search_location_with_radius(radius):
-    try:
-        geolocator = geocoders.Nominatim(user_agent="your_app_name")
-        location = geolocator.geocode("Your search")
-        if location is not None:
-            map_widget.set_position(location.latitude, location.longitude)
-        else:
-            show_error_message("Location not found. Please try again with a different search query.")
-    except GeocoderUnavailable as e:
-        print(f"Error: {e}")
-        show_error_message("Failed to connect to the geocoding service. Please try again later.")
-    except requests.exceptions.ConnectionError as e:
-        print(f"Error: {e}")
-        show_error_message("Failed to connect to the geocoding service. Please try again later.")
+def add_marker_event(coords):
+    marker_dialog = ctk.CTkInputDialog(text="Type in the name of the marker:", title="My Map")
+    markers.append(map_widget.set_marker(coords[0], coords[1], text=marker_dialog.get_input()))
 
 
-root_tk = tk.Tk()
-root_tk.geometry(f"{800}x{600}")
-root_tk.title("map_view_example.py")
+def clear_marker():
+    for marker in markers:
+        marker.delete()
 
-# create map widget
-map_widget = TkinterMapView(root_tk, width=800, height=600, corner_radius=0)
-map_widget.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-# create search button
-search_button = tk.Button(root_tk, text="Search Location", command=open_second_window)
-search_button.pack()
+def path():
+    path_dialog = ctk.CTkInputDialog(
+                                     text="Type in two markers you want to create a path: (Ex: 1-2, 1-3, 2-3,...)",
+                                     title="My Map")
+    chosen_markers = path_dialog.get_input().split("-")
+    chosen_markers[0], chosen_markers[1] = int(chosen_markers[0]) - 1, int(chosen_markers[1]) - 1
+    paths.append(
+        map_widget.set_path([markers[chosen_markers[0]].position, markers[chosen_markers[1]].position], color="green"))
 
-root_tk.mainloop()
+
+def change_mode():
+    if switch.get() == 1:
+        ctk.set_appearance_mode("dark")
+    else:
+        ctk.set_appearance_mode("light")
+
+
+ctk.set_appearance_mode("light")
+ctk.set_default_color_theme("green")
+
+root = ctk.CTk()
+root.geometry("800x600")
+root.title("Lost & Found")
+
+map_widget = tkmap.TkinterMapView(master=root, width=780, height=520, corner_radius=7)
+map_widget.pack()
+map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)
+map_widget.set_zoom(8)
+map_widget.set_address("warsaw")
+map_widget.pack(fill="both", expand=True)
+
+map_widget.add_right_click_menu_command(label="Add marker", command=add_marker_event, pass_coords=True)
+
+frame = ctk.CTkFrame(master=root, width=875, height=60, corner_radius=7)
+frame.pack(padx=10, pady=10)
+
+lookup_btn = ctk.CTkButton(master=frame, text="Lookup", command=lookup, width=20, font=("Segoe UI", 10))
+lookup_btn.grid(row=0, column=0, padx=10)
+
+slider = ctk.CTkSlider(master=frame, from_=0, to=22, command=zoom, width=150)
+slider.grid(row=0, column=1, padx=10)
+
+marker_btn = ctk.CTkButton(master=frame, text="Set marker", command=marker, width=20, font=("Segoe UI", 10))
+marker_btn.grid(row=0, column=2, padx=10)
+
+clear_marker_btn = ctk.CTkButton(master=frame, text="Clear marker", command=clear_marker, width=20,
+                                 font=("Segoe UI", 10))
+clear_marker_btn.grid(row=0, column=3, padx=10)
+
+path_btn = ctk.CTkButton(master=frame, text="Set path", command=path, width=20, font=("Segoe UI", 10))
+path_btn.grid(row=0, column=4, padx=10)
+
+switch = ctk.CTkSwitch(master=frame, text="Dark Mode", command=change_mode)
+switch.grid(row=0, column=5, padx=10)
+
+exit_btn = ctk.CTkButton(master=frame, text="Exit", command=root.destroy, width=20, font=("Segoe UI", 10))
+exit_btn.grid(row=0, column=6, padx=10)
+
+root.mainloop()
